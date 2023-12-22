ce9175e893934ed9144a645e2bec282e935d2ac3
Talha Ansari <talhaansari1606@gmail.com>
2023-12-22 12:57:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Demo from './components/demo'
+import "./App.css"
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+        <div className='gradient'/>
+        <div className='app'>
+          <Hero />
+          <Demo />
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
